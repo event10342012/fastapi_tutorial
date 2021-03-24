31d003f6cdee6a54883c91fc9c294b1cb3a7aef5
S71456 <event-71456@esunsec.com.tw>
2021-03-24 14:24:09 +0800

    feat: add tutorial

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # fastapi_tutorial
+
+### initialize project
+
+
+1. 下載 [sqlite3](https://www.sqlite.org/download.html)
+
+2. 把上面檔案路徑加到環境變數
+
+3. 執行下面語法新增sqlite db
+
+```
+sqlite sqlite.db
+.quit
+```
+
+4. 執行start.py，新增table與輸入範例資料
+
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import sqlite3
 from enum import Enum
 from typing import Optional
 
-import psycopg2
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path
 
 from model import Phone
 
@@ -12,15 +12,15 @@ FILE_PATH = 'phone.csv'
 
 
 def get_conn():
-    return psycopg2.connect(dbname='postgres', user='postgres', password='s71456')
+    return sqlite3.connect('sqlite.db')
 
 
 def get_phone_list():
     sql = 'select * from phone'
     with get_conn() as conn:
-        with conn.cursor() as cursor:
-            cursor.execute(sql)
-            rows = cursor.fetchall()
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        rows = cursor.fetchall()
     phones = [Phone(id=row[0], name=row[1], description=row[2], price=row[3]) for row in rows]
     return phones
 
@@ -38,95 +38,95 @@ async def root():
 
 # Path parameter
 
-# # 1 path
-# @app.get('/phone/{phone_id}')
-# async def get_phone_id(phone_id: int):
-#     phone_list = get_phone_list()
-#     phone: Phone
-#     for phone in phone_list:
-#         if phone.id == phone_id:
-#             return {'name': phone.name, 'description': phone.description, 'price': phone.price}
-#     return {'Phone not found'}
-#
-#
-# # 2 return list
-# @app.get('/iphone')
-# async def get_iphone():
-#     phone_list = get_phone_list()
-#     phone: Phone
-#     result = []
-#     for phone in phone_list:
-#         if phone.name == 'IPhone':
-#             result.append(phone)
-#     return {'result': result}
-#
-#
-# # 3 enum
-# @app.get('/phone_name/{phone_name}')
-# async def get_phone(phone_name: PhoneEnum):
-#     if phone_name == PhoneEnum.samsung:
-#         return {'Phone name': phone_name}
-#
-#     if phone_name.value == 'htc':
-#         return {'Phone name': phone_name}
-#
-#     return {'Phone name': phone_name}
+@app.get('/items/{item_id}')
+async def get_item(item_id: int):
+    return {'item_id': item_id}
+
+
+# 1 path
+@app.get('/phone/{phone_id}')
+async def get_phone_id(phone_id: int):
+    phone_list = get_phone_list()
+    phone: Phone
+    for phone in phone_list:
+        if phone.id == phone_id:
+            return {'name': phone.name, 'description': phone.description, 'price': phone.price}
+    return {'Phone not found'}
+
+
+# 2 return list
+@app.get('/iphone')
+async def get_iphone():
+    phone_list = get_phone_list()
+    phone: Phone
+    result = []
+    for phone in phone_list:
+        if phone.name == 'IPhone':
+            result.append(phone)
+    return {'result': result}
+
+
+# 3 enum
+@app.get('/phone_name/{phone_name}')
+async def get_phone(phone_name: PhoneEnum):
+    if phone_name == PhoneEnum.samsung:
+        return {'Phone name': phone_name}
+
+    if phone_name.value == 'htc':
+        return {'Phone name': phone_name}
+
+    return {'Phone name': phone_name}
 
 # query parameter
 
+
 # # 1 path and query
 # @app.get('/phone/{phone_id}')
-# async def get_phone_id(phone_id: int, tax_pct: Optional[float] = None):
+# async def get_phone_id(phone_id: int, times: Optional[int] = None):
 #     phone_list = get_phone_list()
 #     for phone in phone_list:
 #         if phone.id == phone_id:
-#             if tax_pct:
-#                 phone = phone.dict()
-#                 phone['tax'] = phone['price'] * tax_pct
+#             if times:
+#                 phone.price *= times
 #                 return phone
 #             else:
 #                 return phone
 #
 #     return {'Phone not found'}
-#
-#
-# # 2 任意給訂參數
-# @app.get('/phone')
-# async def get_phone(name: str, price: Optional[int] = None):
-#     phone_list = get_phone_list()
-#     phone: Phone
-#     for phone in phone_list:
-#         if phone.name == name:
-#             price = price or phone.price
-#             return {'name': phone.name, 'price': price}
-#     return {'Phone not found'}
+
 
 # request body
 # 1
-@app.post('/create_phone/')
-async def create_phone(phone: Phone):
-    with get_conn() as conn:
-        with conn.cursor() as cursor:
-            sql = '''
-            insert into phone (name, description, price) values (%s, %s, %s)
-            '''
-            values = (phone.name, phone.description, phone.price)
-            cursor.execute(sql, values)
-        conn.commit()
-    return phone
+# @app.post('/create_phone/')
+# async def create_phone(phone: Phone):
+#     with get_conn() as conn:
+#         cursor = conn.cursor()
+#         sql = '''
+#         insert into phone (id, name, description, price) values (?, ?, ?, ?)
+#         '''
+#         values = (phone.id, phone.name, phone.description, phone.price)
+#         cursor.execute(sql, values)
+#         conn.commit()
+#     return phone
 
 
 # Query: string validation
 # ... make it required
-@app.get('/phone')
-async def get_phone(name: Optional[str] = Query(None, max_length=5)):
-    phone_list = get_phone_list()
-    phone: Phone
-    result = []
-    for phone in phone_list:
-        if phone.name == name:
-            result.append(phone)
-    return result
+# @app.get('/phone')
+# async def get_phone(name: Optional[str] = Query(None, max_length=5, deprecated=True)):
+#     phone_list = get_phone_list()
+#     phone: Phone
+#     result = []
+#     for phone in phone_list:
+#         if phone.name == name:
+#             result.append(phone)
+#     return result
 
 
 # Path: number validation
+# @app.get('/phone/{phone_id}')
+# async def get_phone(phone_id: int = Path(..., title='phone id', ge=0)):
+#     phone_list = get_phone_list()
+#     for phone in phone_list:
+#         if phone.id == phone_id:
+#             return phone
