16a48ae34623c59f98afd4ded221b889c2d32208
S71456 <event-71456@esunsec.com.tw>
2021-04-15 08:41:02 +0800

    feat: add tutorial

--- app/main.py
+++ app/main.py
@@ -4,9 +4,7 @@ from .dependencies import get_query_token, get_token_header
 from .internal import admin
 from .routers import items, users
 
-# global dependency
-# app = FastAPI(dependencies=[Depends(get_query_token)])
-app = FastAPI()
+app = FastAPI(dependencies=[Depends(get_query_token)])
 
 app.include_router(users.router)
 app.include_router(items.router)
